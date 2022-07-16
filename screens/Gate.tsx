a170c85db9900c97c47152432f507e4cd251da7d
Maor Magori <maor@magori.online>
2022-07-16 18:58:44 +0300

    feat: 🚧 Add Gate screen and toolbar naviagtion

---
+++ screens/Gate.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import {Text, View} from 'react-native';
+
+const Gate: React.FC = () => {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>ggg!</Text>
+    </View>
+  );
+};
+
+export default Gate;
